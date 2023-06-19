5e0c88bbf3e8d1c86e5f68acb5eb20ae4d4945a5
paulocvj <paulo.cvj@aluno.ifsc.edu.br>
2023-06-19 11:22:49 -0300

    Correção do terceiro item

--- T3 - Transformada de Fourier/Q3.py
+++ T3 - Transformada de Fourier/Q3.py
@@ -22,10 +22,11 @@ for a, beta in valores_a_beta.items():
     W = MS7P2(beta, a)
 
     # Cálculo do erro percentual em valor absoluto
-    erro_percentual = np.abs((W - valor_referencia) / valor_referencia) * 100
+    erro_percentual = np.abs((W - valor_referencia*beta) / valor_referencia*beta) * 100
 
     # Exibição dos resultados
-    print(f"Largura de banda essencial (a={a}): {W} rad/s")
-    print(f"Erro percentual (a={a}): {erro_percentual}%\n")
+    print(f"Valor de referência com {beta*100}% do sinal: {valor_referencia*beta:.6f}")
+    print(f"Largura de banda essencial (a={a}): {W:.6f} rad/s")
+    print(f"Erro percentual (a={a}): {erro_percentual:.6f}%\n")
 
 input()
