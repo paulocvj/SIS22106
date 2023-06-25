0cfc953f25bb5d0bae74f6849972ba082d3e72be
Paulo Cezar <paulo.cvj@aluno.ifsc.edu.br>
2023-06-25 20:32:18 -0300

    Arrumado erro no push

---
+++ T3 - Transformada de Fourier/Q2-c.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def MS7P1(x):
+    y = np.ones_like(x)
+    i = np.where(x != 0)[0]
+    y[i] = np.sin(x[i]) / x[i]
+    return y
+
+def X(w):
+    delta_t = -1 / 4 * np.sinc(w / (np.pi * 4)) ** 2
+    esq_t = 1 / 2 * (3 * np.sinc(w * 3/ (np.pi * 2)) * np.exp(-1j * w))
+    dir_t = (1 / (w**2)) * (np.exp(-2j * w) + 2j * w * np.exp(-2j *w) - 1) * np.exp(-1j / 2 * w)
+    return dir_t + delta_t + esq_t
+
+omega = np.linspace(-20, 20, 80)
+Dn = X(omega)
+
+
+# Configuração da figura
+fig = plt.figure(figsize=(8, 10))
+
+# Espectro de Magnitude
+plt.subplot(4, 1, 2)
+plt.stem(omega, np.abs(Dn), 'k')
+plt.xlabel('Omega')
+plt.ylabel('|Dn|')
+
+
+# Espectro de Fase
+plt.subplot(4, 1, 3)
+plt.stem(omega, np.angle(Dn), 'k')
+plt.xlabel('Omega')
+plt.ylabel('Ângulo Dn[rad]')
+
+
+plt.tight_layout()  # Ajusta o espaçamento entre os subplots
+plt.show()
