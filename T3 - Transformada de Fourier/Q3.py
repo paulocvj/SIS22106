f365a6a6fd4a9ea5ba2643b032341bad3d215299
paulocvj <paulo.cvj@aluno.ifsc.edu.br>
2023-06-26 08:27:22 -0300

    Modificado o código final da Q3

--- T3 - Transformada de Fourier/Q3.py
+++ T3 - Transformada de Fourier/Q3.py
@@ -1,32 +1,47 @@
 import numpy as np
+from math import pi
+from scipy import integrate
 
-# Definição da função MS7P2
-def MS7P2(beta, a):
-    W = np.tan(beta * np.pi / 2) * a
-    return W
+def calcular_X(w, a):
+    return 1 / (a + 1j * w)
 
-# Valor de referência do livro
-valor_referencia = 12.706
+def calcular_energia(B, a):
+    return B / (2 * a)
 
-# Dicionário com os valores de 'a' e 'beta' correspondentes
-valores_a_beta = {
-    1: 0.95,
-    2: 0.9,
-    3: 0.75
-}
+# Tolerância escolhida
+tolerancia = 0.0000001
+# Pares de valores entre A e B
+valores_ab = [(1, 0.95), (2, 0.90), (3, 0.75)]
 
-print(f'Valor de referência utilizado na análise: {valor_referencia} rad/s\n')
-# Loop para cálculo e exibição dos resultados
-for a, beta in valores_a_beta.items():
-    # Cálculo da largura de banda essencial
-    W = MS7P2(beta, a)
+for a, b in valores_ab:
+    E = calcular_energia(b, a)
+    relerr = (E - 0) / E
+    limite = 100
+    i = 0
+    W = 0
+    passo = pi * a
 
-    # Cálculo do erro percentual em valor absoluto
-    erro_percentual = np.abs((W - valor_referencia*beta) / valor_referencia*beta) * 100
+    E_W, _ = integrate.quad(lambda w: (abs(calcular_X(w, a)) ** 2) / (2 * pi), -np.inf, np.inf)
 
-    # Exibição dos resultados
-    print(f"Valor de referência com {beta*100}% do sinal: {valor_referencia*beta:.6f}")
-    print(f"Largura de banda essencial (a={a}): {W:.6f} rad/s")
-    print(f"Erro percentual (a={a}): {erro_percentual:.6f}%\n")
+    print(f"Para a = {a} e b = {b}")
+    print(f"Energia para a = {a}: {calcular_energia(1, a):.6f}")
+    print(f"E_W: {E_W:.6f}")
+    print(f"E: {E:.6f}")
 
-input()
+    while abs(relerr) > tolerancia:
+        if relerr > 0:
+            W = W + passo
+        elif relerr < 0:
+            passo = passo / 2
+            W = W - passo
+
+        E_W, _ = integrate.quad(lambda w: (abs(calcular_X(w, a)) ** 2) / (2 * pi), -W, W)
+        relerr = (E - E_W) / E
+
+        i += 1
+
+        if i > limite:
+            print(f"Não foi possível determinar W em {i} iterações!\nO valor mais próximo foi W = {W:.6f} com {relerr * 100}% de erro")
+            break
+
+    print(f"{i} iterações totais: W = {W:.6f}\nErro = {relerr * 100:.6f}%\n Energia = {E_W:.6f}, sendo {E_W * 2 * a:.6f}% da energia do sinal\n")
